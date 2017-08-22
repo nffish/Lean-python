051de35cf05ac77bcb6f419fab1cdd6fa5e3da45
Nffish <nffish@gmail.com>
2017-08-22 17:10:11 +0800

    Add do_listcompr.py

---
+++ advance/do_listcompr.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+print([x * x for x in range(1, 11)])
+print(x * x for x in range(1, 11) if x % 2 == 0)
+print([m + n for m in 'ABC' for n in 'XYZ'])
+
+d = {'x': 'A', 'y': 'B', 'z': 'C'}
+print([k + '=' + v for k, v in d.items()])
+
+L = ['Hello', 'world', 'IBM', 'Apple']
+print([s.lower() for s in L])
