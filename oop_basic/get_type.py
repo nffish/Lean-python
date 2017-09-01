b3f1d8bb9c94fec396c5dba0d72ca9ab73a1ef91
Nffish <nffish@gmail.com>
2017-09-01 16:24:46 +0800

    add animals.py

---
+++ oop_basic/get_type.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# type()
+
+print('type(123) =', type(123))
+print('type(\'123\') =', type('123'))
+print('type(None)=', type(None))
+print('type(abs) =', type(abs))
+
+print('type(\'abs\')==str', type('abc') == str)
