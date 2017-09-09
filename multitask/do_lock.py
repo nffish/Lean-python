89792840bec27f854d15abb9c71dc7d3a5f59e5a
nffish <nffish@gmail.com>
2017-09-09 16:18:52 +0800

    add multi_threading.py

---
+++ multitask/do_lock.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import time
+import threading
+
+balance = 0
+lock = threading.Lock()
+
+
+def change_it(n):
+    global balance
+    balance = balance + n
+    balance = balance - n
+
+
+def run_thread(n):
+    for i in range(10000):
+        # 要线获取锁:
+        lock.acquire()
+        try:
+            change_it(n)
+        finally:
+            # 改完一定要释放锁：
+            lock.release()
+
+
+t1 = threading.Thread(target=run_thread, args=(5,))
+t2 = threading.Thread(target=run_thread, args=(8,))
+t1.start()
+t2.start()
+t1.join()
+t2.join()
+print(balance)
